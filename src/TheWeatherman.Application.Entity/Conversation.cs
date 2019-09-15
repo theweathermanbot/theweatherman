d21cbceadee36dede43fcac8a5de521d0de26678
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-15 13:38:57 +0200

    Added User, Chat and Conversation

---
+++ src/TheWeatherman.Application.Entity/Conversation.cs
@@ -0,0 +1,8 @@
+namespace TheWeatherman.Application.Entity
+{
+    public class Conversation
+    {        
+          public Chat Chat { get; set; }
+          public User User { get; set; }
+    }
+}
