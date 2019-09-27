21f592967659dcc6673b153c9d89a42353158f91
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-27 14:34:54 +0200

    Added UpdateController

---
+++ src/TheWeatherman.Api.WebApi/Controllers/UpdateController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace TheWeatherman.Api.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UpdateController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Update update)
+        {
+            var a = 3;
+            return Ok();
+        }
+    }
+}
