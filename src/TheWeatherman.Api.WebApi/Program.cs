886219dffa7a953611cf3dc3de7218da7c950a27
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-27 12:12:01 +0200

    Updated to dotnetcore 3.0

--- src/TheWeatherman.Api.WebApi/Program.cs
+++ src/TheWeatherman.Api.WebApi/Program.cs
@@ -1,44 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog;
-using TheWeatherman.Common.IoC.Configuration;
 
 namespace TheWeatherman.Api.WebApi
 {
     public class Program
     {
-        private static IConfiguration Configuration { get; } = ConfigurationExtension.LoadConfiguration();
-
-        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseSerilog();
-        
         public static void Main(string[] args)
         {
-            Log.Logger = ConfigurationExtension.LoadLogger(Configuration);
-
-            try
-            {
-                Log.Information("Starting TheWeatherman");
-
-                CreateWebHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "TheWeatherman terminated unexpectedly");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
